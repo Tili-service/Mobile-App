e16a96a9cb0cb1df22e271b981438ec7d2aef9d3
Matisse-M <matisse.marsac@epitech.eu>
2026-03-17 17:35:30 +0100

    refactor(app): update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fullscreen/flutter_fullscreen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/tili_root.dart';
 
 void main() async {
   /* Initialize Flutter and the fullscreen plugin before running the app */
   WidgetsFlutterBinding.ensureInitialized();
   await FullScreen.ensureInitialized();
+  try {
+    await dotenv.load();
+  } catch (e) {
+    print('Error loading .env file: $e');
+  }
   FullScreen.setFullScreen(true);
 
   /* Run the main app widget */
